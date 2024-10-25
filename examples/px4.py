bc83f79d9f047efd918ddc09848470abe173a813
Quinn Thibeault <quinn.thibeault96@gmail.com>
2024-10-25 13:50:24 -0700

    Add verbose flag to enable GZCM logging in PX4 example

--- examples/px4.py
+++ examples/px4.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import logging
 import pprint
 
 import click
@@ -9,7 +10,12 @@ import gzcm.gazebo as gz
 
 @click.command("px4")
 @click.option("-c", "--container", default=None)
-def px4_example(container: str | None):
+@click.option("-v", "--verbose", is_flag=True)
+def px4_example(container: str | None, verbose: bool):
+    if verbose:
+        logging.basicConfig()
+        logging.getLogger("gzcm").setLevel(logging.DEBUG)
+
     px4cfg = px4.Config()
     gzcfg = gz.Config()
     poses = px4.simulate(px4cfg, gzcfg, container=container)
