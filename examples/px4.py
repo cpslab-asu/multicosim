02162d04a8d31cc5bdc1db9070f48ed7712096ea
Quinn Thibeault <quinn.thibeault96@gmail.com>
2024-10-28 11:31:07 -0700

    Fix incorrect keyword argument for remove option

--- examples/px4.py
+++ examples/px4.py
@@ -11,7 +11,7 @@ import gzcm.gazebo as gz
 @click.command("px4")
 @click.option("-c", "--container", default=None)
 @click.option("-v", "--verbose", is_flag=True)
-@click.option("--rm", "--remove", "remove", is_flat=True)
+@click.option("--rm", "--remove", "remove", is_flag=True)
 def px4_example(container: str | None, verbose: bool, remove: bool):
     if verbose:
         logging.basicConfig()
