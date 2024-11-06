41eb54d50841a9625363470cdef21fc4f855ba5e
Quinn Thibeault <quinn.thibeault96@gmail.com>
2024-11-05 17:29:56 -0700

    Add helper function to docker module to start firmware containers

--- src/gzcm/docker.py
+++ src/gzcm/docker.py
@@ -1,16 +1,20 @@
 from __future__ import annotations
 
+from collections.abc import Generator
+from contextlib import contextmanager
 from logging import NullHandler, getLogger
 from typing import TypeAlias, TYPE_CHECKING
 
 import docker
 import docker.errors
 import docker.models
+import docker.models.containers
 import docker.models.images
 
 if TYPE_CHECKING:
     Client: TypeAlias = docker.DockerClient
     Image: TypeAlias = docker.models.images.Image
+    Container: TypeAlias = docker.models.containers.Container
 
 
 def ensure_image(name: str, *, client: Client) -> Image:
@@ -32,3 +36,36 @@ def ensure_image(name: str, *, client: Client) -> Image:
 
     return image
 
+
+@contextmanager
+def start_container(
+    image: str,
+    command: str,
+    ports: list[int],
+    *,
+    client: Client,
+    remove: bool = False
+) -> Generator[Container, None, None]:
+    image_ = ensure_image(image, client=client)
+    container = client.containers.run(
+        image=image_,
+        command=command,
+        detach=True,
+        ports={f"{port}/tcp": None for port in ports},
+        tty=True,
+        stdin_open=True,
+    )
+
+    try:
+        yield container
+    finally:
+        container.reload()
+
+        if container.status != "exited":
+            container.kill()
+            container.wait()
+
+        status_code = int(container.attrs['State']['ExitCode'])
+
+        if remove and status_code != 0:
+            container.remove()
