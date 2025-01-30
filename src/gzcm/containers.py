e72cdc22da78e2af98d79cbb79132f4e1eade86a
Quinn Thibeault <quinn.thibeault96@gmail.com>
2025-01-30 14:01:08 -0700

    Refactor containers module and introduce images module

--- src/gzcm/containers.py
+++ src/gzcm/containers.py
@@ -1,43 +1,17 @@
 from __future__ import annotations
 
-from collections.abc import Mapping
 from contextlib import contextmanager
-from logging import NullHandler, getLogger
-from typing import Generator, Literal, TypeAlias
+from typing import TYPE_CHECKING, Generator, Literal, TypeAlias, cast
 
-import docker
-import docker.errors
-import docker.models.containers
-import docker.models.images
+from . import images
 
-Client: TypeAlias = docker.DockerClient
-Image: TypeAlias = docker.models.images.Image
-Container: TypeAlias = docker.models.containers.Container
+if TYPE_CHECKING:
+    from collections.abc import Mapping
+
+    from docker import DockerClient as Client
+    from docker.models.containers import Container
+
 PortProtocol: TypeAlias = Literal["tcp", "udp"]
-
-
-def ensure_image(name: str, *, client: Client) -> Image:
-    logger = getLogger("gzcm.containers")
-    logger.addHandler(NullHandler())
-
-    if ":" not in name:
-        name = f"{name}:latest"
-
-    try:
-        image = client.images.get(name)
-    except docker.errors.ImageNotFound:
-        logger.debug(f"Image {name} not found, pulling...")
-
-        parts = name.split(":")
-        repository = parts[0]
-        tag = parts[1] if len(parts) > 1 else None
-        image = client.images.pull(repository, tag)
-
-        logger.debug(f"Image {name} pulled.")
-    else:
-        logger.debug(f"Image {name} found.")
-
-    return image
 
 
 @contextmanager
@@ -46,6 +20,15 @@ def find(name: str, *, client: Client) -> Generator[Container, None, None]:
         yield client.containers.get(name)
     finally:
         pass
+
+
+class ContainerError(Exception):
+    pass
+
+
+class AbnormalExitError(ContainerError):
+    def __init__(self, name: str, exit_code: int):
+        super().__init__(f"Container {name} exited abnormally with code {exit_code}. Please check the container logs.")
 
 
 @contextmanager
@@ -58,7 +41,7 @@ def start(
     host: Container | None = None,
     remove: bool = False,
 ) -> Generator[Container, None, None]:
-    image_ = ensure_image(image, client=client)
+    image_ = images.ensure(image, client=client)
     container = client.containers.run(
         image=image_,
         command=command,
@@ -69,20 +52,23 @@ def start(
         stdin_open=True,
     )
 
-    try:
-        while container.status == "created":
-            container.reload()
+    while container.status != "running":
+        # Wait for the container to have the proper state before yielding to the user
+        container.reload()
 
+    try:
         yield container
     finally:
         container.reload()
 
-        if container.status == "exited":
-            status_code = int(container.attrs["State"]["ExitCode"])
-            remove = remove and status_code == 0  # Only remove the container if it exited successfully, even if indicated
-        else:
+        if container.status != "exited":
             container.kill()
-            container.wait()
 
-        if remove:
-            container.remove()
+        result = container.wait()
+        exit_code = int(result["StatusCode"])
+        good_exit = exit_code != 0 and exit_code != 143
+
+        if not good_exit:
+            raise AbnormalExitError(cast(str, container.name), exit_code)  # Throw an error if the container exited unsuccessfully
+        elif remove:
+            container.remove()  # If the container exited successfully, remove if indicated
---
+++ src/gzcm/images.py
@@ -0,0 +1,34 @@
+from __future__ import annotations
+
+import logging
+import typing
+
+import docker.errors
+
+if typing.TYPE_CHECKING:
+    from docker import DockerClient as Client
+    from docker.models.images import Image
+
+
+def ensure(name: str, *, client: Client) -> Image:
+    logger = logging.getLogger("gzcm.containers")
+    logger.addHandler(logging.NullHandler())
+
+    if ":" not in name:
+        name = f"{name}:latest"
+
+    try:
+        image = client.images.get(name)
+    except docker.errors.ImageNotFound:
+        logger.debug(f"Image {name} not found, pulling...")
+
+        parts = name.split(":")
+        repository = parts[0]
+        tag = parts[1] if len(parts) > 1 else None
+        image = client.images.pull(repository, tag)
+
+        logger.debug(f"Image {name} pulled.")
+    else:
+        logger.debug(f"Image {name} found.")
+
+    return image
