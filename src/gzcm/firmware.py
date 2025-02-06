9a344379b458268aeb3162d3cfef88a99569e4d8
Quinn Thibeault <quinn.thibeault96@gmail.com>
2025-02-06 09:43:08 -0700

    Allow users to specify the expected return type from the controller
    Users can use the rtype parameter of the firmware decorator to enable
    static and runtime type checking of the return value from the
    controller. This allows the controllers to return their own custom types
    so the library can adapt to any use case.

--- src/gzcm/firmware.py
+++ src/gzcm/firmware.py
@@ -4,7 +4,7 @@ from collections.abc import Generator
 from contextlib import contextmanager
 from dataclasses import dataclass
 from pathlib import Path
-from typing import TYPE_CHECKING, Literal, TypeAlias, TypeVar, TypedDict, Final, Generic, Protocol, ParamSpec, ContextManager
+from typing import TYPE_CHECKING, Literal, TypeAlias, TypeVar, TypedDict, Final, Generic, Protocol, ParamSpec, ContextManager, overload, cast
 
 import attrs
 import docker
@@ -19,23 +19,6 @@ if TYPE_CHECKING:
 
 PortProtocol: TypeAlias = Literal["tcp", "udp"]
 DEFAULT_PORT: Final[int] = 5556
-
-
-@attrs.frozen()
-class Pose:
-    """The pose of the PX4 vehicle in meters."""
-
-    x: float
-    y: float
-    z: float
-
-
-@attrs.frozen()
-class State:
-    """The pose of the PX4 vehicle in meters, along with the associated time-stamp."""
-
-    time: float
-    pose: Pose
 
 
 class SimulationError(Exception):
@@ -73,9 +56,12 @@ def _get_host_port(container: Container, port: int, *, protocol: PortProtocol = 
     raise ValueError("Could not find host port binding")
 
 
+R = TypeVar("R")
+
+
 @attrs.frozen()
-class Success:
-    states: list[State]
+class Success(Generic[R]):
+    value: R
 
 
 @attrs.frozen()
@@ -83,41 +69,19 @@ class Failure:
     error: Exception
 
 
-@attrs.frozen()
-class Waypoint:
-    """A position in a mission plan for a PX4-controlled vehicle.
-
-    Args:
-        lat: The latitude to achieve
-        lon: The longitude to achieve
-        alt: The altitude to achieve
-    """
-
-    lat: float
-    lon: float
-    alt: float
-
-
-Mission: TypeAlias = list[Waypoint]
-
-DEFAULT_MISSION: Final[Mission] = [
-    Waypoint(47.398039859999997, 8.5455725400000002, 25),
-    Waypoint(47.398036222362471, 8.5450146439425509, 25),
-    Waypoint(47.397825620791885, 8.5450092830163271, 25),
-]
-
 P = ParamSpec("P")
 T = TypeVar("T")
 M = TypeVar("M", covariant=True)
 
 
-class Firmware(Generic[T]):
-    def __init__(self, client: Client, container: Container, socket: zmq.Socket):
+class Firmware(Generic[T, R]):
+    def __init__(self, client: Client, container: Container, socket: zmq.Socket, rtype: type[R]):
         self.container = container
         self.socket = socket
         self.client = client
+        self.rtype = rtype
 
-    def run(self, msg: T) -> list[State]:
+    def run(self, msg: T) -> R:
         self.socket.send_pyobj(msg)
 
         while True:
@@ -127,17 +91,23 @@ class Firmware(Generic[T]):
                 raise SimulationError("Container exited without completing mission. Please check container logs for more information.")
 
             try:
-                result = self.socket.recv_pyobj()
+                response = self.socket.recv_pyobj()
             except zmq.ZMQError:
                 continue
 
-            if isinstance(result, Success):
-                return result.states
+            if isinstance(response, Success):
+                if not isinstance(response.value, self.rtype):
+                    raise TypeError(f"Expected response value of type {self.rtype}, got {type(response.value)}")
 
-            if isinstance(result, Failure):
-                raise result.error
+                return response.value
 
-            raise TypeError(f"Unsupported type {type(result)} received from firmware. Expected [Success, Failure].")
+            if isinstance(response, Failure):
+                if not isinstance(response.error, Exception):
+                    raise TypeError(f"Expected error value of type Exception, got {type(response.error)}")
+
+                raise response.error
+
+            raise TypeError(f"Unsupported type {type(response)} received from firmware. Expected [Success, Failure].")
 
     def attach(
         self,
@@ -160,16 +130,17 @@ class Firmware(Generic[T]):
 
 
 @dataclass()
-class FirmwareManager(Generic[M]):
+class FirmwareManager(Generic[M, R]):
     client: Client | None
     context: zmq.Context | None
     firmware_image: str
     command: str
     port: int
     remove: bool
+    rtype: type[R]
 
     @contextmanager
-    def start(self) -> Generator[Firmware[M], None, None]:
+    def start(self) -> Generator[Firmware[M, R], None, None]:
         client = self.client or docker.from_env()
         context = self.context or zmq.Context()
         ctx = gzcm.containers.start(
@@ -189,7 +160,7 @@ class FirmwareManager(Generic[M]):
 
                 with context.socket(zmq.REQ) as socket:
                     with socket.connect(f"tcp://localhost:{port}"):
-                        yield Firmware(client, container, socket)
+                        yield Firmware(client, container, socket, self.rtype)
         finally:
             pass
 
@@ -200,13 +171,13 @@ class MsgFactory(Protocol[P, M]):
 
 
 @dataclass()
-class FirmwareWrapper(Generic[P, M], FirmwareManager[M]):
+class FirmwareWrapper(Generic[P, M, R], FirmwareManager[M, R]):
     msg_factory: MsgFactory[P, M]
     gazebo_image: str
     world: Path
     template: Path
 
-    def run(self, gazebo: gz.Gazebo, remove: bool = False, *args: P.args, **kwargs: P.kwargs) -> list[State]:
+    def run(self, gazebo: gz.Gazebo, remove: bool = False, *args: P.args, **kwargs: P.kwargs) -> R:
         with self.start() as fw:
             sim = fw.attach(
                 gazebo,
@@ -223,23 +194,56 @@ class FirmwareWrapper(Generic[P, M], FirmwareManager[M]):
         return states
 
 
-class Decorator(Protocol):
-    def __call__(self, func: MsgFactory[P, M]) -> FirmwareWrapper[P, M]:
+class Decorator(Protocol[R]):
+    def __call__(self, func: MsgFactory[P, M]) -> FirmwareWrapper[P, M, R]:
         ...
 
 
+@overload
 def manage(
     firmware_image: str,
     gazebo_image: str,
     command: str,
+    rtype: None = ...,
     port: int = 5556,
     world: Path = Path("/tmp/generated.sdf"),
     template: Path = Path("resources/worlds/default.sdf"),
     remove: bool = False,
     client: Client | None = None,
     context: zmq.Context | None = None,
-) -> Decorator:
-    def decorator(func: MsgFactory[P, M]) -> FirmwareWrapper[P, M]:
+) -> Decorator[object]:
+    ...
+
+
+@overload
+def manage(
+    firmware_image: str,
+    gazebo_image: str,
+    command: str,
+    rtype: type[R],
+    port: int = 5556,
+    world: Path = Path("/tmp/generated.sdf"),
+    template: Path = Path("resources/worlds/default.sdf"),
+    remove: bool = False,
+    client: Client | None = None,
+    context: zmq.Context | None = None,
+) -> Decorator[R]:
+    ...
+
+
+def manage(
+    firmware_image: str,
+    gazebo_image: str,
+    command: str,
+    rtype: type[R] | None = None,
+    port: int = 5556,
+    world: Path = Path("/tmp/generated.sdf"),
+    template: Path = Path("resources/worlds/default.sdf"),
+    remove: bool = False,
+    client: Client | None = None,
+    context: zmq.Context | None = None,
+) -> Decorator[R] | Decorator[object]:
+    def decorator(func: MsgFactory[P, M]) -> FirmwareWrapper[P, M, R | object]:
         return FirmwareWrapper(
             client,
             context,
@@ -247,10 +251,14 @@ def manage(
             command,
             port,
             remove,
+            rtype or object,
             func,
             gazebo_image,
             world,
             template,
         )
 
-    return decorator
+    if rtype:
+        return cast(Decorator[R], decorator)
+    
+    return cast(Decorator[object], decorator)
--- src/gzcm/px4.py
+++ src/gzcm/px4.py
@@ -1,27 +1,65 @@
 from __future__ import annotations
 
+from collections.abc import Iterable, Iterator
 from enum import IntEnum
 from pathlib import Path
-from typing import Final, TypeAlias
+from typing import Final
 
 import attrs
 
 from gzcm import gazebo as gz
 from gzcm import firmware as fw
 from gzcm.__about__ import __version__
-from gzcm.firmware import Pose, State, Waypoint
 
-Mission: TypeAlias = list[fw.Waypoint]
-Trace: TypeAlias = dict[float, fw.Pose]
+
+@attrs.frozen()
+class Waypoint:
+    """A position in a mission plan for a PX4-controlled vehicle.
+
+    Args:
+        lat: The latitude to achieve
+        lon: The longitude to achieve
+        alt: The altitude to achieve
+    """
+
+    lat: float
+    lon: float
+    alt: float
+
+
+@attrs.frozen()
+class Pose:
+    """The pose of the PX4 vehicle in meters."""
+
+    x: float
+    y: float
+    z: float
+
+
+@attrs.frozen()
+class State:
+    """The pose of the PX4 vehicle in meters, along with the associated time-stamp."""
+
+    time: float
+    pose: Pose
+
 
 PORT: Final[int] = 5556
 
 
 @attrs.frozen()
 class StartMsg:
-    mission: Mission
+    mission: list[Waypoint]
     model: Model
     world: str
+
+
+@attrs.frozen()
+class States(Iterable[State]):
+    values: list[State]
+
+    def __iter__(self) -> Iterator[State]:
+        return iter(self.values)
 
 
 @fw.manage(
@@ -30,18 +68,19 @@ class StartMsg:
     command=f"/usr/local/bin/firmware --port {PORT} --verbose",
     port=PORT,
     template=Path("resources/worlds/default.sdf"),
+    rtype=States,
 )
-def firmware(world: str, mission: Mission, model: Model) -> StartMsg:
+def firmware(world: str, mission: list[Waypoint], model: Model) -> StartMsg:
     return StartMsg(mission, model, world)
 
 
 def simulate(
-    mission: Mission,
+    mission: list[Waypoint],
     model: Model,
     gazebo: gz.Gazebo,
     *,
     remove: bool = False,
-) -> Trace:
+) -> dict[float, Pose]:
     """Execute a simulation using the PX4 using the given configuration.
 
     Args:
@@ -60,10 +99,10 @@ def simulate(
     return {state.time: state.pose for state in states}
 
 
-DEFAULT_MISSION: Final[Mission] = [
-    fw.Waypoint(47.398039859999997, 8.5455725400000002, 25),
-    fw.Waypoint(47.398036222362471, 8.5450146439425509, 25),
-    fw.Waypoint(47.397825620791885, 8.5450092830163271, 25),
+DEFAULT_MISSION: Final[list[Waypoint]] = [
+    Waypoint(47.398039859999997, 8.5455725400000002, 25),
+    Waypoint(47.398036222362471, 8.5450146439425509, 25),
+    Waypoint(47.397825620791885, 8.5450092830163271, 25),
 ]
 
 
@@ -87,7 +126,13 @@ class PX4:
 
     model: Model = attrs.field(default=Model.X500)
 
-    def simulate(self, gazebo: gz.Gazebo, mission: Mission | None = None, *, remove: bool = False) -> Trace:
+    def simulate(
+        self,
+        gazebo: gz.Gazebo,
+        mission: list[Waypoint] | None = None,
+        *,
+        remove: bool = False
+    ) -> dict[float, Pose]:
         return simulate(mission or DEFAULT_MISSION, self.model, gazebo, remove=remove)
 
 Model = PX4.Model
