2cfb254a30fb839eafa6ebea9b28983d44218231
Quinn Thibeault <quinn.thibeault96@gmail.com>
2025-02-06 09:46:16 -0700

    Remove PX4 from top-level exports because users should import the whole module to use it

--- src/gzcm/__init__.py
+++ src/gzcm/__init__.py
@@ -1,13 +1,9 @@
 from gzcm.__about__ import __version__
-from gzcm.firmware import Waypoint
 from gzcm.gazebo import Gazebo, ODE, Dart, Bullet, Simbody
-from gzcm.px4 import PX4
 
 __all__ = [
     "__version__",
     "Gazebo",
-    "PX4",
-    "Waypoint",
     "ODE",
     "Dart",
     "Bullet",
