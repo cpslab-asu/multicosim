efa82919aade9616ef1b7e87148d82f3dfbf6388
Quinn Thibeault <quinn.thibeault96@gmail.com>
2025-06-01 09:31:51 -0700

    Create decorator to transform a function into a simulator

--- src/multicosim/simulations.py
+++ src/multicosim/simulations.py
@@ -1,12 +1,39 @@
 from __future__ import annotations
 
-from typing import ContextManager
+from collections.abc import Callable, Generator
+from contextlib import contextmanager
+from typing import ContextManager, Generic, TypeVar
+
+from typing_extensions import ParamSpec, override
+
+SimT = TypeVar("SimT")
 
 
-class Simulation:
-    pass
-
-
-class Simulator:
-    def start(self) -> ContextManager[Simulation]:
+class Simulator(Generic[SimT]):
+    def start(self) -> ContextManager[SimT]:
         ...
+
+
+class DefaultSimulator(Simulator[SimT]):
+    def __init__(self, factory: Callable[[], ContextManager[SimT]]):
+        self.factory = factory
+
+    @override
+    def start(self) -> ContextManager[SimT]:
+        return self.factory()
+
+
+P = ParamSpec("P")
+ 
+
+def simulator(func: Callable[P, Generator[SimT, None, None]]) -> Callable[P, Simulator[SimT]]:
+    """Transform a generator into a simulator instance."""
+
+    def _wrapper(*args: P.args, **kwargs: P.kwargs) -> Simulator[SimT]:
+        @contextmanager
+        def _inner() -> Generator[SimT, None, None]:
+            return func(*args, **kwargs)
+
+        return DefaultSimulator(_inner)
+
+    return _wrapper
