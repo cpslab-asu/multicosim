bfe61e6bf8554e49eb9a495ae99d332238ef246d
Quinn Thibeault <quinn.thibeault96@gmail.com>
2025-06-11 13:24:52 -0700

    Implement Component and Node for firmware containers that communicate with their simulation components

--- src/multicosim/firmware.py
+++ src/multicosim/firmware.py
@@ -2,22 +2,17 @@ from __future__ import annotations
 
 import logging
 from collections.abc import Callable, Generator
-from contextlib import contextmanager
-from dataclasses import dataclass
-from pathlib import Path
-from typing import TYPE_CHECKING, TypeVar, TypedDict, Final, Generic, Protocol, ContextManager, overload, cast
+from contextlib import ExitStack, contextmanager
+from typing import TYPE_CHECKING, Final, Generic, Protocol, TypedDict, TypeVar
 
 import attrs
-import docker
-import zmq
 import typing_extensions
+import zmq
 
-from . import containers
-from . import gazebo as gz
+from .docker import Environment, ReporterComponent, ReporterNode
+from .simulations import CommunicationNode, Component
 
 if TYPE_CHECKING:
-    from docker import DockerClient as Client
-
     from .containers import Container, PortProtocol
 
 DEFAULT_PORT: Final[int] = 5556
@@ -32,11 +27,9 @@ class MessageError(SimulationError):
 
 
 def _has_port_mappings(container: Container, *ports: int, protocol: PortProtocol = "tcp") -> bool:
-    for port in ports:
-        if not container.ports.get(f"{port}/{protocol}"):
-            return False
-
-    return True
+    return all(
+        container.ports.get(f"{port}/{protocol}") is not None for port in ports
+    )
 
 
 class PortMapping(TypedDict):
@@ -58,253 +51,156 @@ def _get_host_port(container: Container, port: int, *, protocol: PortProtocol = 
     raise ValueError("Could not find host port binding")
 
 
-R = TypeVar("R", covariant=True)
+DataT = TypeVar("DataT", covariant=True)
+MsgT = TypeVar("MsgT", contravariant=True)
 
 
 @attrs.frozen()
-class Success(Generic[R]):
-    value: R
+class Success(Generic[DataT]):
+    data: DataT
 
 
 @attrs.frozen()
 class Failure:
-    error: Exception
+    msg: str
 
 
-P = typing_extensions.ParamSpec("P")
-T = TypeVar("T")
-M = TypeVar("M", covariant=True)
-
-
-class Process(Generic[T, R]):
-    def __init__(self, client: Client, container: Container, socket: zmq.Socket, rtype: type[R]):
-        self.container = container
-        self.socket = socket
-        self.client = client
-        self.rtype = rtype
-
-    def run(self, msg: T) -> R:
-        self.socket.send_pyobj(msg)
-
-        while True:
-            self.container.reload()
-
-            if self.container.status == "exited":
-                raise SimulationError("Container exited without completing mission. Please check container logs for more information.")
-
-            try:
-                response = self.socket.recv_pyobj()
-            except zmq.ZMQError:
-                continue
-
-            if isinstance(response, Success):
-                if not isinstance(response.value, self.rtype):
-                    raise TypeError(f"Expected response value of type {self.rtype}, got {type(response.value)}")
-
-                return response.value
-
-            if isinstance(response, Failure):
-                if not isinstance(response.error, Exception):
-                    raise TypeError(f"Expected error value of type Exception, got {type(response.error)}")
-
-                raise response.error
-
-            raise TypeError(f"Unsupported type {type(response)} received from firmware. Expected [Success, Failure].")
-
-    def attach(
-        self,
-        gazebo: gz.Gazebo,
-        image: str,
-        world: Path,
-        template: Path,
-        *,
-        remove: bool = False
-    ) -> ContextManager[gz.Simulation]:
-        return gz.start(
-            config=gazebo,
-            host=self.container,
-            image=image,
-            base=template,
-            world=world,
-            client=self.client,
-            remove=remove,
-        )
-
-
-@dataclass()
-class Firmware(Generic[M, R]):
-    client: Client | None
-    context: zmq.Context | None
-    firmware_image: str
-    command: str
-    port: int
-    remove: bool
-    rtype: type[R]
-
-    @contextmanager
-    def start(self) -> Generator[Process[M, R], None, None]:
-        client = self.client or docker.from_env()
-        context = self.context or zmq.Context()
-        ctx = containers.start(
-            client=client,
-            image=self.firmware_image,
-            command=self.command,
-            ports={self.port: "tcp"},
-            remove=self.remove,
-        )
+@contextmanager
+def _transport_socket(port: int) -> Generator[zmq.Socket, None, None]:
+    with ExitStack() as stack:
+        ctx = stack.enter_context(zmq.Context())
+        sock = stack.enter_context(ctx.socket(zmq.REP))
+        sock_ = stack.enter_context(sock.bind(f"tcp://*:{port}"))
 
         try:
-            with ctx as container:
-                while not _has_port_mappings(container, self.port):
-                    container.reload()
-
-                port = _get_host_port(container, self.port)
-
-                with context.socket(zmq.REQ) as socket:
-                    with socket.connect(f"tcp://localhost:{port}"):
-                        yield Process(client, container, socket, self.rtype)
+            yield sock_
         finally:
             pass
 
 
-class MsgFactory(Protocol[P, M]):
-    def __call__(self, world: str, *args: P.args, **kwargs: P.kwargs) -> M:
-        ...
+class FirmwareServer(Generic[MsgT, DataT]):
+    def __init__(self, func: Callable[[MsgT], DataT], msgtype: type[MsgT] | None = None):
+        self.msgtype = msgtype
+        self.func = func
 
+    def __call__(self, msg: MsgT) -> DataT:
+        if self.msgtype and not isinstance(msg, self.msgtype):
+            raise TypeError(f"Unexpected argument type {type(msg)}, expected {self.msgtype}")
 
-@dataclass()
-class ManagedFirmware(Generic[P, M, R], Firmware[M, R]):
-    msg_factory: MsgFactory[P, M]
-    gazebo_image: str
-    world: Path
-    template: Path
+        return self.func(msg)
 
-    def run(self, gazebo: gz.Gazebo, remove: bool = False, *args: P.args, **kwargs: P.kwargs) -> R:
-        with self.start() as fw:
-            sim = fw.attach(
-                gazebo,
-                self.gazebo_image,
-                self.world,
-                self.template,
-                remove=remove,
-            )
+    def listen(self, port: int = DEFAULT_PORT):
+        with _transport_socket(port) as socket:
+            logger = logging.getLogger("multicosim.program")
+            logger.addHandler(logging.NullHandler())
+            logger.debug("Waiting for configuration message...")
 
-            with sim:
-                msg = self.msg_factory(self.world.stem, *args, **kwargs)
-                states = fw.run(msg)
+            msg = socket.recv_pyobj()
 
-        return states
+            if self.msgtype is not None and not isinstance(msg, self.msgtype):
+                raise TypeError(f"Unknown start message type {type(msg)}. Expected {self.msgtype}")
 
+            logger.debug("Received configuration message. Running firmware...")
 
-class Decorator(Protocol[R]):
-    def __call__(self, func: MsgFactory[P, M]) -> ManagedFirmware[P, M, R]:
-        ...
+            try:
+                result: Success[DataT] | Failure = Success(self.func(msg))
+            except Exception as e:
+                result = Failure(str(e))
 
-
-@overload
-def manage(
-    firmware_image: str,
-    gazebo_image: str,
-    command: str,
-    rtype: None = ...,
-    port: int = 5556,
-    world: Path = Path("/tmp/generated.sdf"),
-    template: Path = Path("resources/worlds/default.sdf"),
-    remove: bool = False,
-    client: Client | None = None,
-    context: zmq.Context | None = None,
-) -> Decorator[object]:
-    ...
-
-
-@overload
-def manage(
-    firmware_image: str,
-    gazebo_image: str,
-    command: str,
-    rtype: type[R],
-    port: int = 5556,
-    world: Path = Path("/tmp/generated.sdf"),
-    template: Path = Path("resources/worlds/default.sdf"),
-    remove: bool = False,
-    client: Client | None = None,
-    context: zmq.Context | None = None,
-) -> Decorator[R]:
-    ...
-
-
-def manage(
-    firmware_image: str,
-    gazebo_image: str,
-    command: str,
-    rtype: type[R] | None = None,
-    port: int = 5556,
-    world: Path = Path("/tmp/generated.sdf"),
-    template: Path = Path("resources/worlds/default.sdf"),
-    remove: bool = False,
-    client: Client | None = None,
-    context: zmq.Context | None = None,
-) -> Decorator[R] | Decorator[object]:
-    def decorator(func: MsgFactory[P, M]) -> ManagedFirmware[P, M, R | object]:
-        return ManagedFirmware(
-            client,
-            context,
-            firmware_image,
-            command,
-            port,
-            remove,
-            rtype or object,
-            func,
-            gazebo_image,
-            world,
-            template,
-        )
-
-    if rtype:
-        return cast(Decorator[R], decorator)
-    
-    return cast(Decorator[object], decorator)
+            socket.send_pyobj(result)
 
 
 A = TypeVar("A")
-B = TypeVar("B", covariant=True)
 
 
-class Server(Generic[B]):
-    def __init__(self, func: Callable[[A], B], msgtype: type[A] | None = None):
-        self.msgtype = msgtype
-        self.func = func
-        self.logger = logging.getLogger("multicosim.program")
-        self.logger.addHandler(logging.NullHandler())
-
-    def __call__(self, port: int = DEFAULT_PORT):
-        with zmq.Context() as ctx:
-            with ctx.socket(zmq.REP) as socket:
-                with socket.bind(f"tcp://*:{port}"):
-                    self.logger.debug("Waiting for configuration message...")
-
-                    msg = socket.recv_pyobj()
-
-                    if self.msgtype is not None and not isinstance(msg, self.msgtype):
-                        raise TypeError(f"Unknown start message type {type(msg)}. Expected {self.msgtype}")
-
-                    try:
-                        result: Success[B] | Failure = Success(self.func(msg))
-                    except Exception as e:
-                        result = Failure(e)
-
-                    socket.send_pyobj(result)
-
-
-C = TypeVar("C")
-D = TypeVar("D")
-E = TypeVar("E")
-
-
-class ProgramDecorator(Protocol):
-    def __call__(self, __func: Callable[[A], C], /) -> Server[C]:
+class FirmwareDecorator(Protocol[A]):
+    def __call__(self, func: Callable[[A], DataT]) -> FirmwareServer[A, DataT]:
         ...
 
 
-def serve(*, msgtype: type[A] | None = None) -> ProgramDecorator:
-    return lambda f: Server(f, msgtype)
+def firmware(*, msgtype: type[MsgT]) -> FirmwareDecorator[MsgT]:
+    def decorator(func: Callable[[MsgT], DataT]) -> FirmwareServer[MsgT, DataT]:
+        return FirmwareServer(func, msgtype)
+
+    return decorator
+
+
+class FirmwareError(Exception):
+    def __init__(self, msg: str):
+        super().__init__(f"Error during firmware execution: {msg}")
+
+
+class ResponseDataTypeError(Exception):
+    def __init__(self, actual: object, expected: type):
+        super().__init__(f"Unsupported response data type {type(actual)}, expected {expected}")
+
+
+class ResponseTypeError(Exception):
+    def __init__(self, response: object):
+        super().__init__(
+            f"Unsupported response type {type(response)}, expected {type(Success)} or {type(Failure)}"
+        )
+
+
+def _extract_response_data(response: object, data_type: type[DataT]) -> DataT:
+    if isinstance(response, Failure):
+        raise FirmwareError(response.msg)
+
+    if isinstance(response, Success):
+        if isinstance(response.data, data_type):
+            return response.data
+        
+        raise ResponseDataTypeError(response.data, data_type)
+
+    raise ResponseTypeError(response)
+
+
+@attrs.define()
+class FirmwareContainerNode(CommunicationNode[MsgT, DataT]):
+    node: ReporterNode
+    message_type: type[MsgT]
+    response_type: type[DataT]
+
+    @typing_extensions.override
+    def send(self, msg: MsgT) -> DataT:
+        if not isinstance(msg, self.message_type):
+            raise TypeError(f"Unsupported message type {type(msg)}, expected {self.message_type}")
+
+        return _extract_response_data(self.node.send(msg), self.response_type)
+
+    @typing_extensions.override
+    def stop(self):
+        self.node.stop()
+
+
+@attrs.define()
+class FirmwareContainerComponent(Component[Environment, FirmwareContainerNode[MsgT, DataT]]):
+    """A component representing the firmware/controller of a system.
+
+    Args:
+        image: The docker image to use for execution
+        command: The command to execute in the container
+        port: The port to use for communication with the controller
+    """
+
+    def __init__(
+        self,
+        image: str,
+        command: str,
+        port: int,
+        message_type: type[MsgT],
+        response_type: type[DataT],
+        *,
+        remove: bool = False
+    ):
+        self.component = ReporterComponent(image, command, port, remove=remove)
+        self.message_type = message_type
+        self.response_type = response_type
+
+    @typing_extensions.override
+    def start(self, environment: Environment) -> FirmwareContainerNode[MsgT, DataT]:
+        return FirmwareContainerNode(
+            self.component.start(environment),
+            self.message_type,
+            self.response_type,
+        )
