7a62b395d7b393e701ae479ccf3d2f2e46a1cb71
Quinn Thibeault <quinn.thibeault96@gmail.com>
2025-06-17 09:38:36 -0700

    Use __about__ module for version to avoid uninitialized module error from px4 for importing __version__ symbol

--- src/multicosim/__init__.py
+++ src/multicosim/__init__.py
@@ -1,8 +1,7 @@
+from .__about__ import __version__
 from .docker import ContainerComponent
 from .gazebo import ODE, Bullet, Dart, Simbody
 from .px4 import PX4
-
-__version__ = "0.2.0"
 
 __all__ = [
     "__version__",
