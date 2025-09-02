d48183efb449450d25bd576322c434ee391eafae
Quinn Thibeault <quinn.thibeault96@gmail.com>
2025-09-01 23:19:05 -0700

    Update firmware driver program to configure ArduPilot networking endpoints

--- ardupilot/src/firmware.py
+++ ardupilot/src/firmware.py
@@ -16,12 +16,17 @@ import multicosim.ardupilot as ap
 import multicosim.docker.firmware as fw
 
 
-def run_ardupilot(vehicle: ap.Vehicle, frame: str, param_files: list[pathlib.Path]):
+def run_ardupilot(
+    vehicle: ap.Vehicle,
+    frame: str,
+    gazebo_host: str,
+    firmware_host: str,
+    param_files: list[pathlib.Path],
+):
     logger = logging.getLogger("ardupilot.firmware.run")
     logger.addHandler(logging.NullHandler())
 
     workdir = pathlib.Path("/opt/ardupilot")
-    
     sim_cmd = "./Tools/autotest/sim_vehicle.py --no-configure --no-rebuild "
 
     match vehicle:
@@ -36,7 +41,7 @@ def run_ardupilot(vehicle: ap.Vehicle, frame: str, param_files: list[pathlib.Pat
         case _:
             raise Exception("Invalid ardupilot vehicle type!")
 
-    sim_cmd += f"-f {frame} --model JSON --sim-address=gazebo_harmonic --out=tcpin:ardupilot_sitl:14551"
+    sim_cmd += f"-f {frame} --model JSON --sim-address={gazebo_host} --out=tcpin:{firmware_host}:14551"
 
     for pfile in param_files:
         sim_cmd += f" --add-param-file {pfile}"
@@ -45,12 +50,18 @@ def run_ardupilot(vehicle: ap.Vehicle, frame: str, param_files: list[pathlib.Pat
     return subprocess.Popen(sim_cmd, cwd=workdir, shell=True, encoding="utf-8")
 
 
-def start_ardupilot(vehicle: ap.Vehicle, frame: str, param_files: list[pathlib.Path]):
+def start_ardupilot(
+    vehicle: ap.Vehicle,
+    frame: str,
+    gazebo_host: str,
+    firmware_host: str,
+    param_files: list[pathlib.Path],
+):
     logger = logging.getLogger("ardupilot.firmware")
     logger.addHandler(logging.NullHandler())
     logger.debug("Running ArduPilot firmware")
 
-    process = run_ardupilot(vehicle, frame, param_files)
+    process = run_ardupilot(vehicle, frame, gazebo_host, firmware_host, param_files)
 
     def shutdown():
         process.kill()
@@ -75,17 +86,18 @@ def start_ardupilot(vehicle: ap.Vehicle, frame: str, param_files: list[pathlib.P
 
 @fw.firmware(msgtype=ap.Start)
 def server(msg: ap.Start) -> ap.Result:
-    start_ardupilot(msg.vehicle, msg.frame, msg.param_files)
+    return start_ardupilot(msg.vehicle, msg.frame, msg.gazebo_host, msg.firmware_host, msg.param_files)
 
 
 @click.group()
 @click.option("--verbose", is_flag=True)
-def firmware(*,verbose: bool):
-    if verbose:
-        logging.basicConfig(level=logging.DEBUG)
+def firmware(*, verbose: bool):
+    logging.basicConfig(
+        level=logging.DEBUG if verbose else logging.INFO,
+    )
 
 
-@firmware.command("run")
+@firmware.command()
 @click.option("--vehicle", type=click.Choice(ap.Vehicle, case_sensitive=False), default=ap.Vehicle.COPTER)
 @click.option("--frame", type=str, default="quad")
 @click.option("--param-file", type=click.Path(), multiple=True)
@@ -93,7 +105,7 @@ def run(vehicle: ap.Vehicle, frame: str, param_files: Sequence[pathlib.Path]):
     start_ardupilot(vehicle, frame, list(param_files))
 
 
-@firmware.command("listen")
+@firmware.command()
 @click.option("--port", type=int, default=5556)
 def listen(port: int):
     server.listen(port)
