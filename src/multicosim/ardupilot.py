7cc08f43891143ea64c2f83ec4e295f7d4315d9f
Quinn Thibeault <quinn.thibeault96@gmail.com>
2025-09-01 23:16:25 -0700

    Refactor Ardupilot implementations to favor composition over inheritance
    Define additional fields on Start message to configure networking
    endpoints for ardupilot.

--- src/multicosim/ardupilot.py
+++ src/multicosim/ardupilot.py
@@ -1,22 +1,17 @@
 from __future__ import annotations
 
-from typing import Final, Any
-
-from attrs import field, frozen, define
 from enum import Enum
-from typing_extensions import override
+from typing import Final
 
+from attrs import field, frozen
+
+from . import simulations as _sims
 from .__about__ import __version__
-from .docker.firmware import (
-    FirmwareConfig,
-    GazeboFirmwareSimulator,
-    JointGazeboFirmwareComponent,
-    JointGazeboFirmwareNode,
-)
-from .docker.gazebo import BaseGazeboConfig, GazeboConfig, GazeboContainerNode
-from .docker.simulation import Environment
+from .docker import firmware as _fw
+from .docker import gazebo as _gz
 
 PORT: Final[int] = 5556
+
 
 class Vehicle(Enum):
     NONE = 0
@@ -38,12 +33,22 @@ class Vehicle(Enum):
             case 4:
                 return "sub"
 
+
+@frozen()
+class ArduPilotOptions:
+    vehicle: Vehicle = field(default=Vehicle.COPTER)
+    frame: str = field(default="quad")
+    param_files: list[str] = field(factory=list)
+
+
 @frozen()
 class Start:
     vehicle: Vehicle = field()
-    frame: str = field(default="")
-    world: str = field(default="")
-    param_files: list[str] = field(factory=list)
+    frame: str = field()
+    param_files: list[str] = field()
+    gazebo_host: str = field()
+    firmware_host: str = field()
+
 
 @frozen()
 class Pose:
@@ -51,14 +56,17 @@ class Pose:
     y: float = field()
     z: float = field()
 
+
 @frozen()
 class State:
     time: float = field()
     pose: Pose = field()
 
+
 @frozen()
 class Result:
     trajectory: list[State] = field()
+
 
 def _get_image(image: str):
     if image == "" or image == None:
@@ -66,11 +74,41 @@ def _get_image(image: str):
     else:
         return image
 
-class ArduPilotComponent(JointGazeboFirmwareComponent):
+
+class ArduPilotFirmwareNode(_sims.CommunicationNode[ArduPilotOptions, Result]):
+    def __init__(self, node: _fw.JointGazeboFirmwareNode[Start, Result]):
+        self._node = node
+
+    def send(self, msg: ArduPilotOptions) -> Result:
+        gazebo = self._node.gazebo
+        firmware = self._node.firmware
+        msg_ = Start(
+            vehicle=msg.vehicle,
+            frame=msg.frame,
+            param_files=msg.param_files,
+            gazebo_host=gazebo.node.name(),
+            firmware_host=firmware.node.name(),
+        )
+
+        return firmware.send(msg_)
+
+    def stop(self):
+        # Only stop the firmware node because this node is supposed to only represent the firmware
+        # component even though we store the joint component in order to access the gazebo information
+        return self._node.firmware.stop()
+
+
+class ArduPilotGazeboComponent(_fw.JointGazeboFirmwareComponent):
     def __init__(
-        self, gazebo: GazeboConfig, *, name: str = "", vehicle: Vehicle = Vehicle.NONE, frame: str = "", remove: bool = False
+        self,
+        gazebo: _gz.GazeboConfig,
+        *,
+        name: str = "",
+        vehicle: Vehicle = Vehicle.NONE,
+        frame: str = "",
+        remove: bool = False,
     ):
-        gz = GazeboConfig(
+        gz = _gz.GazeboConfig(
             image=_get_image(gazebo.image),
             template=gazebo.template,
             world=gazebo.world,
@@ -87,7 +125,7 @@ class ArduPilotComponent(JointGazeboFirmwareComponent):
         else:
             command += f"listen --port {PORT}"
 
-        fw = FirmwareConfig(
+        fw = _fw.FirmwareConfig(
             image=f"ghcr.io/cpslab-asu/multicosim/ardupilot/firmware:{__version__}",
             command=command,
             port=PORT,
@@ -101,15 +139,48 @@ class ArduPilotComponent(JointGazeboFirmwareComponent):
 
         super().__init__(gz, fw)
 
-    def start(self, environment: Environment) -> JointGazeboFirmwareNode:
-        return JointGazeboFirmwareNode(
+    def start(self, environment: _fw.Environment) -> _fw.JointGazeboFirmwareNode:
+        return _fw.JointGazeboFirmwareNode(
             self.gazebo.start(environment),
             self.firmware.start(environment),
         )
 
 
-class ArduPilot(GazeboFirmwareSimulator):
+class Simulation(_sims.Simulation):
+    def __init__(self, simulation: _fw.GazeboFirmwareSimulation):
+        self.inner = simulation
+
+    @property
+    def gazebo(self) -> _gz.GazeboContainerNode:
+        return self.inner.gazebo
+
+    @property
+    def firmware(self) -> ArduPilotFirmwareNode:
+        return ArduPilotFirmwareNode(self.inner.simulation.get(self.inner.node_id))
+
+    def stop(self):
+        return self.inner.stop()
+
+
+class Simulator(_sims.Simulator[_fw.Environment, Simulation]):
     def __init__(
-        self, gazebo: BaseGazeboConfig, *, name: str = "", vehicle: Vehicle = Vehicle.NONE, frame: str = "", remove: bool = False
+        self,
+        gazebo: _gz.GazeboConfig,
+        *,
+        name: str = "",
+        vehicle: Vehicle = Vehicle.NONE,
+        frame: str = "",
+        remove: bool = False,
     ):
-        super().__init__(ArduPilotComponent(gazebo, name=name, vehicle=vehicle, frame=frame, remove=remove))
+        self.component = ArduPilotGazeboComponent(
+            gazebo=gazebo,
+            name=name,
+            vehicle=vehicle,
+            frame=frame,
+            remove=remove,
+        )
+        self.simulator = _fw.GazeboFirmwareSimulator(self.component)
+
+    def start(self) -> Simulation:
+        return Simulation(self.simulator.start())
+
