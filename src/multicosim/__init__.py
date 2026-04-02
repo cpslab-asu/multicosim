f3eeac00092c05013bd1c960a6ae385f81ce7506
Quinn Thibeault <quinn.thibeault96@gmail.com>
2026-04-01 20:16:40 -0700

    Remove Ardupilot and PX4 exports from multicosim module, relying on importing sub-modules instead.

--- src/multicosim/__init__.py
+++ src/multicosim/__init__.py
@@ -1,21 +1,19 @@
 from .__about__ import __version__
-from .ardupilot import Simulator as ArduPilot
 from .docker.component import ContainerComponent
 from .docker.gazebo import ODE, Bullet, Dart, Simbody
-from .px4 import Simulator as PX4
+from .docker.gazebo import GazeboContainerComponent as Gazebo
 from .simulations import Component, Node, NodeId, Simulation, Simulator
 
 __all__ = [
     "__version__",
-    "ArduPilot",
     "Component",
     "Bullet",
     "ContainerComponent",
     "Dart",
+    "Gazebo",
     "Node",
     "NodeId",
     "ODE",
-    "PX4",
     "Simbody",
     "Simulation",
     "Simulator",
