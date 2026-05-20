54c12524dc0ca344102375c2d8df69bee9be4c47
Quinn Thibeault <quinn.thibeault96@gmail.com>
2026-05-20 09:16:10 -0700

    Update top-level module exports

--- src/multicosim/__init__.py
+++ src/multicosim/__init__.py
@@ -1,20 +1,9 @@
 from .__about__ import __version__
-from .docker.component import ContainerComponent
-from .docker.gazebo import ODE, Bullet, Dart, Simbody
-from .docker.gazebo import GazeboContainerComponent as Gazebo
-from .simulations import Component, Node, NodeId, Simulation, Simulator
+from .simulations import Component, Simulation, Simulator
 
 __all__ = [
     "__version__",
     "Component",
-    "Bullet",
-    "ContainerComponent",
-    "Dart",
-    "Gazebo",
-    "Node",
-    "NodeId",
-    "ODE",
-    "Simbody",
-    "Simulation",
     "Simulator",
+    "Simulation",
 ]
